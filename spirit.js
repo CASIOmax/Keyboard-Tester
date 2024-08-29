d91776166304e2562d1a3d8cb6cd60a61ee2d157
Sajjad Ahmad <112914285+CASIOmax@users.noreply.github.com>
2024-08-29 16:40:51 +0000

    Done

--- spirit.js
+++ spirit.js
@@ -1,7 +1,27 @@
-const body = document.querySelector('body');
-console.log(body)
 
-body.addEventListener('click',function(){
-    console.log("Body Clickeed")
-    document.body.style.backgroundColor="green";
-})
+const colorGen=function(){
+    const hex='0123456789ABCDEF';
+    let color='#';
+    for (let i = 0; i< 6; i++) {
+      color+=hex[Math.floor(Math.random()*16)];         //The math .random operation is like it gives out an array entery to hex and it iterates over the 16 digits it has to create a hex code after 6 iterations
+    }
+    return color;
+}
+const insert=document.getElementById('insert')
+window.addEventListener('keydown',function(e){
+    const rand=colorGen();
+    insert.innerHTML=`
+    <table style="border: 1px solid ${rand};">
+    <tr>
+      <th style="border: 1px solid ${rand};">key</th>
+      <th style="border: 1px solid ${rand};">keyCode</th>
+      <th style="border: 1px solid ${rand};">Code</th>
+    </tr>
+    <tr>
+      <td style="border: 1px solid ${rand};">${e.key === " " ?"Space":e.key}</td>
+      <td style="border: 1px solid ${rand};">${e.keyCode}</td>
+      <td style="border: 1px solid ${rand};">${e.code}</td>
+    </tr>
+  </table>`
+  insert.style.color="white";
+});
